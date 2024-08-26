4261d92c647f2f23b4db7b354c95344bd1b8d9fa
Tushar <tusharmali017@gmail.com>
2024-08-26 13:32:33 +0530

    Version 1.0.0
    Completed-
    
    - Implemented a code for testing the PIR Motion sensor using PIC24FJ128GA606 controller.
    - Tested when it detects IR/IV light, LED turned ON and digital data received on controller pin.
    - Collected Data sends through MODBUS protocol to Planetary Subunit(GUI).

---
+++ version.h
@@ -0,0 +1,23 @@
+/* 
+ * File:   version.h
+ * Author: tushar.mali
+ *
+ * Created on 26 August, 2024, 12:41 PM
+ */
+
+#ifndef VERSION_H
+#define	VERSION_H
+
+#ifdef	__cplusplus
+extern "C" {
+#endif
+
+
+
+
+#ifdef	__cplusplus
+}
+#endif
+
+#endif	/* VERSION_H */
+
